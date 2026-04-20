4b192fcf702c89ca48f6ff6f4e325174e5452d19
Akshat Swami <143736434+Akshatswami610@users.noreply.github.com>
2026-04-20 19:33:34 +0530

    Update urls.py

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,23 +1,8 @@
-from django.contrib.auth.views import LogoutView
-from django.urls import path, include
-from django.contrib import admin
-from rest_framework import routers
-from accounts.views import(
-    RegisterView,
-    LoginView, )
-
-router = routers.DefaultRouter()
-router.register('users', RegisterView)
-router.register('login', LoginView)
-router.register('logout', LogoutView)
-
+from django.urls import path
+from .views import RegisterView, LoginView, LogoutAPIView
 
 urlpatterns = [
-    path('', include('django.contrib.auth.urls')),
-    path('admin/', admin.site.urls),
-    path('', include(router.urls)),
-    path('accounts/', include('django.contrib.auth.urls')),
-    path('login/', LoginView.as_view(template_name='accounts/login.html'), name='login'),
-    path('logout/', LogoutView.as_view(), name='logout'),
     path('register/', RegisterView.as_view(), name='register'),
+    path('login/', LoginView.as_view(), name='login'),
+    path('logout/', LogoutAPIView.as_view(), name='logout'),
 ]
