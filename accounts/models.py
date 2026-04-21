06447213fda8576e1d3870a3fe367e62745651f9
Akshat Swami <143736434+Akshatswami610@users.noreply.github.com>
2026-04-21 22:33:55 +0530

    Update models.py

--- accounts/models.py
+++ accounts/models.py
@@ -2,12 +2,8 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
-    phone_number = models.CharField(
-        max_length=10,
-        unique=True,
-        blank=True,
-        null=True,
-    )
+    phone_number = models.CharField(max_length=10, unique=True, blank=True, null=True)
+    company_name = models.CharField(max_length=255, blank=True, null=True)
 
     def __str__(self):
-        return self.username
+        return self.email
