e4476543de0d659241b7aab2a51d182977b062bf
Akshat Swami <143736434+Akshatswami610@users.noreply.github.com>
2026-04-22 19:16:46 +0530

    Update admin.py

--- listings/admin.py
+++ listings/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from .models import PG, PGImage
 # Register your models here.
+@admin.register(PG)
+class PGAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description')
+    search_fields = ('name', 'description')
+
+@admin.register(PGImage)
+class PGImageAdmin(admin.ModelAdmin):
+    list_display = ('pg',)
