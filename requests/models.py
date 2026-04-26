567f2cf96e43dce046a01733d57425f16afd6ea9
Akshat Swami <143736434+Akshatswami610@users.noreply.github.com>
2026-04-26 18:05:14 +0530

    Update models.py

--- requests/models.py
+++ requests/models.py
@@ -1,14 +1,19 @@
 from django.db import models
 from django.conf import settings
+from django.utils import timezone
 
 User = settings.AUTH_USER_MODEL
 
 class Requests(models.Model):
+    STATUS_CHOICES = [
+        ('pending', 'Pending'),
+        ('accepted', 'Accepted'), ]
     name = models.CharField(max_length=100)
     phone_number = models.CharField(max_length=10)
     pg_id = models.CharField(max_length=100, default='')
-    date = models.DateField()
-    time = models.TimeField()
-
+    visit_date = models.DateField()
+    visit_time = models.TimeField()
+    status = models.CharField(choices=STATUS_CHOICES, default='pending')
+    timestamp = models.DateTimeField(default=timezone.now)
     def __str__(self):
         return self.name
