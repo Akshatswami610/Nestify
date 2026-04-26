b2520282acd70995432f434db9cb73e69f6cf212
Akshat Swami <143736434+Akshatswami610@users.noreply.github.com>
2026-04-26 15:30:26 +0530

    Update admin.py

--- requests/admin.py
+++ requests/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Requests
 
-# Register your models here.
+@admin.register(Requests)
+class RequestsAdmin(admin.ModelAdmin):
+    list_display = ('id','name', 'phone_number', 'date')
+    search_fields = ('name', 'phone_number', 'date')
