d88b4f667ee9ea7e74cf5967b1f50b720a6f065e
Akshat Swami <143736434+Akshatswami610@users.noreply.github.com>
2026-04-26 15:30:36 +0530

    Update models.py

--- requests/models.py
+++ requests/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+User = settings.AUTH_USER_MODEL
+
+class Requests(models.Model):
+    name = models.CharField(max_length=100)
+    phone_number = models.CharField(max_length=10)
+
+    date = models.DateField()
+    time = models.TimeField()
+
+    def __str__(self):
+        return self.name
+
