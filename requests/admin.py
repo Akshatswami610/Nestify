079bc7b9d979b665d3a008187a8936cf83411df1
Akshat Swami <143736434+Akshatswami610@users.noreply.github.com>
2026-04-26 18:05:10 +0530

    Update admin.py

--- requests/admin.py
+++ requests/admin.py
@@ -3,5 +3,5 @@ from .models import Requests
 
 @admin.register(Requests)
 class RequestsAdmin(admin.ModelAdmin):
-    list_display = ('id','name', 'phone_number', 'date')
-    search_fields = ('name', 'phone_number', 'date')
+    list_display = ('id','name', 'phone_number', 'visit_date','status')
+    search_fields = ('name', 'phone_number', 'visit_date','status')
