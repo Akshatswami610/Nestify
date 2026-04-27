83592c7b8672547cc2c8e9183abea5511bdf7e20
Akshat Swami <143736434+Akshatswami610@users.noreply.github.com>
2026-04-27 20:25:06 +0530

    Update serializers.py

--- listings/serializers.py
+++ listings/serializers.py
@@ -40,5 +40,31 @@ class PGSerializer(serializers.ModelSerializer):
             'is_available',
             'created_at',
             'images',           # for GET
-            'uploaded_images',  # for POST
+            'uploaded_images',  # for POST/PUT
         ]
+
+    # ✅ CREATE with images
+    def create(self, validated_data):
+        uploaded_images = validated_data.pop('uploaded_images', [])
+        pg = PG.objects.create(**validated_data)
+
+        for image in uploaded_images:
+            PGImage.objects.create(pg=pg, image=image)
+
+        return pg
+
+    # ✅ UPDATE with optional new images
+    def update(self, instance, validated_data):
+        uploaded_images = validated_data.pop('uploaded_images', [])
+
+        # update PG fields
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+
+        instance.save()
+
+        # add new images (does NOT delete old ones)
+        for image in uploaded_images:
+            PGImage.objects.create(pg=instance, image=image)
+
+        return instance
