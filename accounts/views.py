c7c27a79604f29bedd1becad7ba4c613d7592ee8
Akshat Swami <143736434+Akshatswami610@users.noreply.github.com>
2026-04-27 19:54:25 +0530

    Update views.py

--- accounts/views.py
+++ accounts/views.py
@@ -2,10 +2,30 @@ from rest_framework import generics, permissions, status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.authtoken.models import Token
-
+from rest_framework.permissions import IsAuthenticated
 from .serializers import RegisterSerializer, LoginSerializer, UserSerializer
 
 
+class MeView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        user = request.user
+        return Response({
+            "username": user.username,
+            "email": user.email,
+            "phone_number": getattr(user, 'phone_number', ''),
+            "company_name": getattr(user, 'company_name', ''),
+        })
+
+    def patch(self, request):
+        user = request.user
+        user.username = request.data.get('username', user.username)
+        user.phone_number = request.data.get('phone_number', user.phone_number)
+        user.company_name = request.data.get('company_name', user.company_name)
+        user.save()
+
+        return Response({"success": True})
 # -----------------------------
 # REGISTER VIEW
 # -----------------------------
