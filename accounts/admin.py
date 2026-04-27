4746450ae68a96d2a79498ac02693ec9cab6ec68
Akshat Swami <143736434+Akshatswami610@users.noreply.github.com>
2026-04-27 20:07:38 +0530

    Update admin.py

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,7 +1,21 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from .models import User
 
+
 @admin.register(User)
-class UserAdmin(admin.ModelAdmin):
-    list_display = ('username', 'email', 'phone_number')
+class UserAdmin(BaseUserAdmin):
+    list_display = ('username', 'email', 'phone_number', 'is_staff')
     search_fields = ('username', 'email', 'phone_number')
+
+    fieldsets = BaseUserAdmin.fieldsets + (
+        ('Additional Info', {
+            'fields': ('phone_number', 'company_name'),
+        }),
+    )
+
+    add_fieldsets = BaseUserAdmin.add_fieldsets + (
+        ('Additional Info', {
+            'fields': ('phone_number', 'company_name'),
+        }),
+    )
