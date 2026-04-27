e224ecd9fdb35e2e8248929af7c4d0dcc6f44781
Akshat Swami <143736434+Akshatswami610@users.noreply.github.com>
2026-04-27 19:54:22 +0530

    Update urls.py

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import RegisterView, LoginView, LogoutAPIView
+from .views import RegisterView, LoginView, LogoutAPIView, MeView
 
 app_name = 'accounts'  # important for namespacing
 
@@ -7,4 +7,5 @@ urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', LoginView.as_view(), name='api-login'),
     path('logout/', LogoutAPIView.as_view(), name='api-logout'),
+    path('me/', MeView.as_view(), name='me'),
 ]
