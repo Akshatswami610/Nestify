bcd3e88cf999e8291c3fbbb5feffcc01e9628b12
Akshat Swami <143736434+Akshatswami610@users.noreply.github.com>
2026-04-29 20:02:09 +0530

    Update views.py

--- requests/views.py
+++ requests/views.py
@@ -1,9 +1,11 @@
 from rest_framework import viewsets, permissions
-from rest_framework.exceptions import PermissionDenied
 from .models import Requests
 from .serializers import RequestsSerializer
 
 class RequestsViewSet(viewsets.ModelViewSet):
     serializer_class = RequestsSerializer
-    permission_classes = [permissions.AllowAny]
-    queryset = Requests.objects.all()
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        return Requests.objects.filter(pg__owner=user)
