be088bccb333c76be4afb25bdb4f096b1bcd1cb9
Akshat Swami <143736434+Akshatswami610@users.noreply.github.com>
2026-04-29 20:35:43 +0530

    Update admin.py

--- support/admin.py
+++ support/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from support.models import Support
+from .models import Support
 
+@admin.register(Support)
 class SupportAdmin(admin.ModelAdmin):
     list_display = ('name', 'email', 'timestamp')
     list_filter = ('email', 'phone_number')
