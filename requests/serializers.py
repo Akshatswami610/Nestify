3777577afa968ef6881dad089c0362bdbaa759c7
Akshat Swami <143736434+Akshatswami610@users.noreply.github.com>
2026-04-29 19:40:24 +0530

    Update serializers.py

--- requests/serializers.py
+++ requests/serializers.py
@@ -1,8 +1,20 @@
 from rest_framework import serializers
 from .models import Requests
 
-
 class RequestsSerializer(serializers.ModelSerializer):
+    pg_id = serializers.IntegerField(source='pg.id', read_only=True)
+    pg_name = serializers.CharField(source='pg.name', read_only=True)
+
     class Meta:
         model = Requests
-        fields = ['name', 'phone_number', 'pg','visit_date','visit_time','status']
+        fields = [
+            'id',
+            'name',
+            'phone_number',
+            'pg_id',
+            'pg_name',
+            'visit_date',
+            'visit_time',
+            'status',
+            'timestamp',
+        ]
