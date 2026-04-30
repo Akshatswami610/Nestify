31d110993ef6e189c1b1a08868d1261b4d826b0f
Akshat Swami <143736434+Akshatswami610@users.noreply.github.com>
2026-04-30 19:30:02 +0530

    Update urls.py

--- listings/urls.py
+++ listings/urls.py
@@ -1,9 +1,10 @@
 from rest_framework.routers import DefaultRouter
-from .views import PGViewSet
+from .views import PGViewSet, PGImageViewSet
 
 app_name = 'listings'
 
 router = DefaultRouter()
 router.register(r'', PGViewSet, basename='pg')
+router.register(r'images', PGImageViewSet, basename='pg-images')
 
 urlpatterns = router.urls  
