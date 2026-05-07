304f72b40d57d525403951af01f080e75dcc40ec
Akshat Swami <143736434+Akshatswami610@users.noreply.github.com>
2026-05-07 23:22:22 +0530

    Update storage_backends.py

--- core/storage_backends.py
+++ core/storage_backends.py
@@ -5,7 +5,6 @@ from decouple import config
 class AzureMediaStorage(AzureStorage):
     account_name = config("AZURE_ACCOUNT_NAME")
     account_key = config("AZURE_ACCOUNT_KEY")
-
     azure_container = "media"
 
     expiration_secs = None
